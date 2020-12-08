1883c5951d356808d5eda1a39c6443d45431a5fd
wpp <1164399367@qq.com>
2020-12-08 17:31:51 +0800

    赶紧保存

---
+++ app/src/main/java/com/whf/messagerelayer/application/App.java
@@ -0,0 +1,18 @@
+package com.whf.messagerelayer.application;
+
+import android.app.*;
+
+import com.whf.messagerelayer.service.TraceServiceImpl;
+import com.xdandroid.hellodaemon.*;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //需要在 Application 的 onCreate() 中调用一次 DaemonEnv.initialize()
+        DaemonEnv.initialize(this, TraceServiceImpl.class, DaemonEnv.DEFAULT_WAKE_UP_INTERVAL);
+        TraceServiceImpl.sShouldStopService = false;
+        DaemonEnv.startServiceMayBind(TraceServiceImpl.class);
+    }
+}
